993452495fa72c8f36f701d92adc0f0ffd344759
mrp-v2 <matthrewp@gmail.com>
2024-09-14 19:49:06 -0600

    creating processed classes

---
+++ VisualSatisfactoryCalculator/satisfactory/model/production/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VisualSatisfactoryCalculator.model.production;
+using VisualSatisfactoryCalculator.satisfactory.Numbers;
+
+namespace VisualSatisfactoryCalculator.satisfactory.model.production
+{
+	public class Recipe : BasicRecipe<Item>
+	{
+		public readonly string id;
+		public readonly string displayName;
+		public readonly string conversionString;
+
+		public Recipe(string id, string displayName, string conversionString, RationalNumber time, ImmutableDictionary<Item, RationalNumber> ingredients, ImmutableDictionary<Item, RationalNumber> products) : base(time, ingredients, products)
+		{
+			this.id = id;
+			this.displayName = displayName;
+			this.conversionString = conversionString;
+		}
+	}
+}
